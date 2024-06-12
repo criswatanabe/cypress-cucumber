23e447c7f5851066e31bf1a1a9c6f4fcf135ade9
Cristiane Watanabe <cwatanabefc@gmail.com>
2024-06-12 09:02:38 -0300

    alteração do nome password

--- cypress/support/pages/register_user_page.js
+++ cypress/support/pages/register_user_page.js
@@ -3,7 +3,7 @@ const elements = {
     fields:{
         name:"#user",
         email:"#email",
-        password:"#password"
+        password:"#password123"
     },
     buttons:{
         register:"#btnRegister"
